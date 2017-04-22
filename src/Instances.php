53d8b6a29dd85983ff5b2da38a0c722c027f0e8e
litepubl <yarrowsoft@gmail.com>
2017-04-22 21:57:26 +0300

    rc7.08 factories: fallback and null

--- src/Instances.php
+++ src/Instances.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace litepubl\core\instances;
 
 use Psr\Container\ContainerInterface;
@@ -8,40 +7,48 @@ class Instances implements ContainerInterface
 {
     protected $DI;
     protected $eventManager;
+    protected $fallbackFactory;
     protected $items;
     protected $circleNames;
 
-    public function __construct(DI $DI, $eventManager)
+    public function __construct(DI $DI, $eventManager, string $fallbackFactory = NullFactory::class)
     {
         $this->DI = $DI;
         $this->eventManager = $eventManager;
-        $this->items = [];
+        $this->fallbackFactory = $fallbackFactory;
         $this->circleNames = [];
+        $this->items = [
+            get_class($DI) => $DI,
+            get_class($this) => $this,
+            'di' => $DI,
+            'container' => $this,
+            'services' => $this
+        ];
     }
 
     public function get($className)
     {
         $className = ltrim($className, '\\');
         if (isset($this->items[$className])) {
-                return $this->items[$className];
+            return $this->items[$className];
         }
-
+        
         if (!class_exists($className)) {
             throw new NotFound(sprintf('Class "%s" not found', $className));
         }
-
+        
         if (in_array($className, $this->circleNames)) {
-                throw new Exception(sprintf('Class "%s" has circle dependencies, current classes stack ', $className, implode("\n", $this->circleNames)));
+            throw new Exception(sprintf('Class "%s" has circle dependencies, current classes stack ', $className, implode("\n", $this->circleNames)));
         }
-
+        
         $this->circleNames[] = $className;
         try {
-                $result = $this->createInstance($className);
-                $this->items[$className] = $result;
+            $result = $this->createInstance($className);
+            $this->items[$className] = $result;
         } finally {
-                array_pop($this->circleNames);
+            array_pop($this->circleNames);
         }
-
+        
         return $result;
     }
 
@@ -62,13 +69,19 @@ class Instances implements ContainerInterface
     public function createInstance(string $className)
     {
         if (interface_exists($className)) {
-                $result = $this->get($this->getImplementation($className));
-        } elseif ($factory = $this->getFactory($className)) {
-                $result = $factory->get($className);
+            $result = $this->get($this->getImplementation($className));
         } else {
-                $result = $this->DI->createInstance($className, [$this, 'get']);
+            $factory = $this->getFactory($className);
+            if ($factory->has($className)) {
+                $result = $factory->get($className);
+            } else {
+                $result = $this->DI->createInstance($className, [
+                    $this,
+                    'get'
+                ]);
+            }
         }
-
+        
         return $result;
     }
 
@@ -77,17 +90,33 @@ class Instances implements ContainerInterface
         if (isset($this->implementations[$interfaceName])) {
             $result = $this->implementations[$interfaceName];
         } else {
-            $a = $this->eventManager->call(static::ONINTERFACE, ['interfaceName' => $interfaceName, 'result' => null]);
+            $a = $this->eventManager->call(static::ONINTERFACE, [
+                'interfaceName' => $interfaceName,
+                'result' => null
+            ]);
             $result = $a['result'];
             if (!$result) {
                 throw new NotFound(sprintf('Interface implementation of "%s" not found', $interfaceName));
             }
         }
-
+        
         return $result;
     }
 
-    public function getFactory(string $className)
+    public function getFactory(string $className): ContainerInterface
     {
+        if (isset($this->factories[$className])) {
+            $result = $this->get($this->factories[$className]);
+        } else {
+            $ns = substr($className, 0, strrpos($className, '\\'));
+            $factoryClass = $ns . '\Factory';
+            if (($className != $factoryClass) && class_exists($factoryClass)) {
+                $result = $this->get($factoryClass);
+            } else {
+                $result = $this->get($this->fallbackFactory);
+            }
+        }
+        
+        return $result;
     }
 }
---
+++ src/NullFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace litepubl\core\instances;
+
+use Psr\Container\ContainerInterface;
+
+class NullFactory implements ContainerInterface
+{
+    public function get($className)
+    {
+            throw new NotFound(sprintf('Class "%s" not found', $className));
+    }
+
+    public function has($className)
+    {
+        return false;
+    }
+}
